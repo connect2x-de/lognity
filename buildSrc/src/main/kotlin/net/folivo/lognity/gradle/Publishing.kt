dbbd7199f2b784e30c2dfc4686206438aa8bfbd7
KitsuneAlex <alexh@karmakrafts.dev>
2025-10-13 08:33:22 +0200

    Add buildSrc to replace external Gradle conventions

---
+++ buildSrc/src/main/kotlin/net/folivo/lognity/gradle/Publishing.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2025 Trixnity
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.folivo.lognity.gradle
+
+import org.gradle.api.publish.PublishingExtension
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.kotlin.dsl.withType
+
+fun PublishingExtension.setProjectInfo( // @formatter:off
+    name: String,
+    description: String,
+    url: String = "https://gitlab.com/trixnity/$name"
+) { // @formatter:on
+    publications.withType<MavenPublication>().configureEach {
+        pom {
+            this.name.set(name)
+            this.description.set(description)
+            this.url.set(url)
+        }
+    }
+}
