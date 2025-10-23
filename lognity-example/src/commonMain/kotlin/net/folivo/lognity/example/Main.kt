9e530f24854462b19f8a4d79e57e5c682e7bcabf
KitsuneAlex <alexh@karmakrafts.dev>
2025-10-23 07:27:46 +0200

    Remove copyright headers from lognity-example module & API refactor

--- lognity-example/src/commonMain/kotlin/net/folivo/lognity/example/Main.kt
+++ lognity-example/src/commonMain/kotlin/net/folivo/lognity/example/Main.kt
@@ -1,24 +1,8 @@
-/*
- * Copyright 2025 Trixnity
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package net.folivo.lognity.example
 
 import kotlinx.io.files.Path
-import net.folivo.lognity.api.Logger
 import net.folivo.lognity.api.backend.Backend
+import net.folivo.lognity.api.logger.Logger
 import net.folivo.lognity.backend.DefaultBackend
 import net.folivo.lognity.config.loadDefaultConfig
 
@@ -32,5 +16,4 @@ fun main() {
     logger.warn { "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam" }
     logger.error { "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam" }
     logger.fatal { "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam" }
-    Backend.current.shutdown() // Shutdown backend, ensure resources are closed
 }
