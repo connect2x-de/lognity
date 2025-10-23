c2035c100f392d7d1f7ed7e461b8b288751cbaa0
KitsuneAlex <alexh@karmakrafts.dev>
2025-10-23 07:26:15 +0200

    Remove copyright headers from lognity-config module & API refactor

--- lognity-config/src/commonMain/kotlin/net/folivo/lognity/config/BackendExtensions.kt
+++ lognity-config/src/commonMain/kotlin/net/folivo/lognity/config/BackendExtensions.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright 2025 Trixnity
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package net.folivo.lognity.config
 
 import kotlinx.io.Source
@@ -28,7 +12,7 @@ fun Backend.loadDefaultConfig( // @formatter:off
     source: Source,
     formatters: Map<String, Formatter> = mapOf("default" to SimpleFormatter.default)
 ) { // @formatter:on
-    defaultConfigSpec = ConfigLoader.load(source, formatters)
+    configSpec = ConfigLoader.load(source, formatters)
 }
 
 fun Backend.loadDefaultConfig( // @formatter:off
