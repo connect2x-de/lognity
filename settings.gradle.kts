c9b88294e84f7b0b31fb96405b27df5717a302ae
KitsuneAlex <alexh@karmakrafts.dev>
2026-01-26 20:32:01 +0100

    Update conventions plugin to the latest version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ dependencyResolutionManagement {
 }
 
 plugins {
-    id("de.connect2x.conventions.c2x-settings-plugin") version "20260125.035218"
+    id("de.connect2x.conventions.c2x-settings-plugin") version "20260126.180426"
 }
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
